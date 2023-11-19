eb0f4be77ac7e399b35bffe9888b0dcc49c85063
joshlim01 <joshlim01@gmail.com>
2023-11-19 12:08:52 -0800

    Cleaned the extension look and added light/dark mode

--- popup.js
+++ popup.js
@@ -27,3 +27,23 @@ document.getElementById("changeFontVerdana").addEventListener("click", function 
     chrome.tabs.sendMessage(tabs[0].id, { action: "changeFont", font: "Verdana, sans-serif" });
   });
 });
+
+document.getElementById("toggleSwitch").addEventListener("change", function () {
+  const body = document.body;
+  const title = document.getElementById("title");
+  const subtitle = document.getElementById("subtitle");
+  const isDarkMode = this.checked;
+
+  if (isDarkMode) {
+    body.style.backgroundColor = "#333";
+    body.style.color = "#fff";
+    title.style.color = "#fff"; // Set title font color to white in dark mode
+    subtitle.style.color = "#fff"; // Set subtitle font color to white in dark mode
+  } else {
+    body.style.backgroundColor = "";
+    body.style.color = "";
+    title.style.color = "black"; // Set title font color to black in light mode
+    subtitle.style.color = "black"; // Set subtitle font color to black in light mode
+  }
+});
+
